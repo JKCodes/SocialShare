3d235b991cf2c08cb7495fbda50c5af629d211da
Joe Kim <josephkim0224@gmail.com>
2017-02-20 09:38:55 -0500

    Very Basic UI Finished

---
+++ SocialShare/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  SocialShare
+//
+//  Created by Joseph Kim on 2/20/17.
+//  Copyright © 2017 Joseph Kim. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SocialShare/PostVC.swift
@@ -0,0 +1,17 @@
+//
+//  PostVC.swift
+//  SocialShare
+//
+//  Created by Joseph Kim on 2/20/17.
+//  Copyright © 2017 Joseph Kim. All rights reserved.
+//
+
+import UIKit
+
+class PostVC: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
